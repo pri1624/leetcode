bdd62b63c81e54955888f2ecd300003ebe95e2a5
Priyanka P <154542659+pri1624@users.noreply.github.com>
2024-10-11 20:54:17 +0530

    Time: 14 ms (10.39%), Space: 8.7 MB (93.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        
-        for(int i=0;i<s.length();i++){
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++){
             if(s[i]!=t[i]){
-                return false;
-            }
+                 return false;
+             }
         }
         return true;
+       
     }
 };
