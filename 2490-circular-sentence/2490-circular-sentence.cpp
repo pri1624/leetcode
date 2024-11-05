259981a4aab4f27911a0f37362162022114e6ec4
Priyanka P <154542659+pri1624@users.noreply.github.com>
2024-11-05 19:34:33 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (81.20%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n=sentence.size();
+        if (sentence[n - 1] != sentence[0]) {
+            return false;
+        }
+        
+        for(int i=1;i<n-1;i++){
+            if(sentence[i]==' '){ 
+                if(sentence[i-1]!=sentence[i+1]){
+                  return false;  
+                }
+            }
+        }
+        return true;
+    }
+};
